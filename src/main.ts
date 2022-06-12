c5d17e298ecf8d3e388d509e0bd84fcdf64a5d88
Kaffee <tobi.von.massow@gmail.com>
2022-06-12 15:44:58 +0200

    add zomber

--- src/main.ts
+++ src/main.ts
@@ -59,10 +59,20 @@ async function game() {
   const PLAYER_H = 70
   const PLAYER_ANIMATION_SPEED_IDLE = 0.05
   const PLAYER_ANIMATION_SPEED_RUN = 0.2
+  const PLAYER_ANIMATION_SPEED_ATTACK = 0.3
   const PLAYER_DRAG_CONSTANT = 1.4
   const PLAYER_VELOCITY_CAP = 4
   const PLAYER_ACCELERATION = 2.8
-  const PLAYER_ATTACK_ANIMATION_SPEED = 0.3
+
+  // ZOMBIE
+  const ZOMBIE_W = 70
+  const ZOMBIE_H = 70
+  const ZOMBIE_ANIMATION_SPEED_IDLE = 0.7
+  const ZOMBIE_ANIMATION_SPEED_RUN = 0.2
+  const ZOMBIE_ANIMATION_SPEED_ATTACK = 0.3
+  const ZOMBIE_DRAG_CONSTANT = 1.4
+  const ZOMBIE_VELOCITY_CAP = 4
+  const ZOMBIE_ACCELERATION = 1.2
 
   // KEYBOARD
   const KEYBOARD_UPDATES_PS = 10
@@ -298,6 +308,110 @@ async function game() {
     keyboard[e.key] = false
   })
 
+  class Zombie {
+    public zombie_container: PIXI.Container
+    public footprints_container: PIXI.Container
+    public footprints_queue: PIXI.Sprite[]
+    public pos: Vec
+    public size: Vec
+    public vel: Vec
+    public attacking: false
+    public shadow_sprite: PIXI.Sprite
+    public idle_sprite: PIXI.AnimatedSprite
+    public run_f_sprite: PIXI.AnimatedSprite
+    public run_r_sprite: PIXI.AnimatedSprite
+    public run_l_sprite: PIXI.AnimatedSprite
+    public run_b_sprite: PIXI.AnimatedSprite
+    public f_attack_sprite: PIXI.AnimatedSprite
+    public active_sprite: PIXI.AnimatedSprite
+    constructor(pos: Vec, size: Vec, vel: Vec) {
+      this.pos = pos
+      this.size = size
+      this.vel = vel
+      this.zombie_container = new PIXI.Container()
+      this.footprints_container = new PIXI.Container()
+      this.footprints_queue = []
+    }
+    async init() {
+      this.shadow_sprite = new PIXI.Sprite(player_shadow_texture)
+      this.zombie_container.addChild(this.shadow_sprite)
+      this.idle_sprite = new PIXI.AnimatedSprite(await loadAnimatedSprite("zombie/", "idle"))
+      this.run_f_sprite = new PIXI.AnimatedSprite(await loadAnimatedSprite("zombie/", "run_f"))
+      this.run_r_sprite = new PIXI.AnimatedSprite(await loadAnimatedSprite("zombie/", "run_r"))
+      this.run_l_sprite = new PIXI.AnimatedSprite(await loadAnimatedSprite("zombie/", "run_l"))
+      this.run_b_sprite = new PIXI.AnimatedSprite(await loadAnimatedSprite("zombie/", "run_b"))
+      this.setActiveAnim("idle_sprite", ZOMBIE_ANIMATION_SPEED_IDLE)
+      this.draw()
+    }
+    controller() {
+      // AI
+      if (this.pos.x > player.pos.x) this.moveBy(new Vec(-ZOMBIE_ACCELERATION, 0))
+      if (this.pos.x < player.pos.x) this.moveBy(new Vec(ZOMBIE_ACCELERATION, 0))
+      if (this.pos.y > player.pos.y) this.moveBy(new Vec(0, -ZOMBIE_ACCELERATION))
+      if (this.pos.y < player.pos.y) this.moveBy(new Vec(0, ZOMBIE_ACCELERATION))
+    }
+    update() {
+      const next_pos = this.pos.add(this.vel)
+      this.pos = next_pos
+
+      // apply natural slow down
+      if (this.vel.x > 0 && this.vel.x > this.vel.y) {
+        if (this.active_sprite !== this.run_l_sprite && !this.attacking) this.setActiveAnim("run_r_sprite", ZOMBIE_ANIMATION_SPEED_RUN)
+      }
+      if (this.vel.y > 0 && this.vel.y > this.vel.x) {
+        if (this.active_sprite !== this.run_f_sprite && !this.attacking) this.setActiveAnim("run_f_sprite", ZOMBIE_ANIMATION_SPEED_RUN)
+      }
+      if (this.vel.x < 0 && this.vel.x < this.vel.y) {
+        if (this.active_sprite !== this.run_r_sprite && !this.attacking) this.setActiveAnim("run_l_sprite", ZOMBIE_ANIMATION_SPEED_RUN)
+      }
+      if (this.vel.y < 0 && this.vel.y < this.vel.x) {
+        if (this.active_sprite !== this.run_b_sprite && !this.attacking) this.setActiveAnim("run_b_sprite", ZOMBIE_ANIMATION_SPEED_RUN)
+      }
+      if (this.vel.x === 0 && this.vel.y === 0 && this.active_sprite !== this.idle_sprite && !this.attacking) this.setActiveAnim("idle_sprite", ZOMBIE_ANIMATION_SPEED_IDLE)
+
+      this.zombie_container.removeChild(this.footprints_container)
+      if (this.footprints_queue.length > 20) {
+        this.footprints_container.removeChild(this.footprints_queue[0])
+        this.footprints_queue.shift()
+      }
+      if (rd() > 0.9 && (this.vel.x !== 0 || this.vel.y !== 0)) {
+        const new_footprint = new PIXI.Sprite(footprints_texture)
+        new_footprint.x = this.pos.x + this.size.x / 2
+        new_footprint.y = this.pos.y + this.size.y / 2
+        this.footprints_container.addChild(new_footprint)
+        this.footprints_queue.push(new_footprint)
+      }
+      this.zombie_container.addChild(this.footprints_container)
+    }
+    moveTo(vec: Vec) {
+      this.pos = this.pos.add(vec)
+    }
+    moveBy(vec: Vec) {
+      this.vel = this.vel.add(vec)
+      // apply vel cap
+      if (this.vel.x > ZOMBIE_VELOCITY_CAP) this.vel.x = ZOMBIE_VELOCITY_CAP
+      if (this.vel.y > ZOMBIE_VELOCITY_CAP) this.vel.y = ZOMBIE_VELOCITY_CAP
+      if (this.vel.x < -ZOMBIE_VELOCITY_CAP) this.vel.x = -ZOMBIE_VELOCITY_CAP
+      if (this.vel.y < -ZOMBIE_VELOCITY_CAP) this.vel.y = -ZOMBIE_VELOCITY_CAP
+    }
+    setActiveAnim(name: string, animation_speed: number) {
+      this.zombie_container.removeChild(this.active_sprite)
+      this.active_sprite = this[name]
+      this.zombie_container.addChild(this.active_sprite)
+      this.active_sprite.animationSpeed = animation_speed
+      this.active_sprite.loop = true
+      this.active_sprite.play()
+    }
+    draw() {
+      this.shadow_sprite.x = this.pos.x + this.size.x / 4
+      this.shadow_sprite.y = this.pos.y + this.size.y * 0.65
+      this.active_sprite.x = this.pos.x
+      this.active_sprite.y = this.pos.y
+      this.active_sprite.width = this.size.x
+      this.active_sprite.height = this.size.y
+    }
+  }
+
   let lineContainer = new PIXI.Container()
   const player_container = new PIXI.Container()
   // create player (death)
@@ -357,7 +471,7 @@ async function game() {
         player_container.removeChild(player.active_sprite)
         player.active_sprite = player.f_attack_sprite
         player.active_sprite.loop = false
-        player.active_sprite.animationSpeed = PLAYER_ATTACK_ANIMATION_SPEED
+        player.active_sprite.animationSpeed = PLAYER_ANIMATION_SPEED_ATTACK
         player.active_sprite.onComplete = () => {
           player_container.removeChild(player.active_sprite)
           player.attacking = false
@@ -369,7 +483,6 @@ async function game() {
     },
     update: () => {
       const next_pos = player.pos.add(player.vel)
-
       collision_debug_container.removeChild(lineContainer)
       lineContainer = new PIXI.Container()
       let collision_detected = false
@@ -386,7 +499,6 @@ async function game() {
             collision_detected = true
           }
           else color = 0xffffff
-
           if (debug_on) {
             let line = new PIXI.Graphics();
             lineContainer.addChild(line);
@@ -409,7 +521,6 @@ async function game() {
         lineContainer.addChild(next_pos_circ)
         collision_debug_container.addChild(lineContainer)
       }
-
       if (!collision_detected) {
         player_container.removeChild(footprints_container)
         if (footprints_queue.length > 20) {
@@ -448,6 +559,8 @@ async function game() {
   }, 200)
 
   setInterval(() => {
+    zomb_1.controller()
+
     // apply natural slow down
     if (player.vel.x > 0) {
       if (player.vel.x < PLAYER_DRAG_CONSTANT) player.vel.x = 0
@@ -489,6 +602,9 @@ async function game() {
   }
 
   app.stage.addChild(background_container)
+  const zomb_1 = new Zombie(new Vec(100, 100), new Vec(PLAYER_W, PLAYER_H), new Vec(0, 0))
+  await zomb_1.init()
+  app.stage.addChild(zomb_1.zombie_container)
   app.stage.addChild(player_container)
   player.init()
 
@@ -503,8 +619,12 @@ async function game() {
     }
   })
 
+
   app.ticker.add((delta: number) => {
     ticks++
+
+    zomb_1.update()
+    zomb_1.draw()
 
     player.update()
     player.draw()
